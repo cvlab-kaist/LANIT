d5ae3beb1571e4fa5c72e1b1d99de0f8b479a229
SunwooKim <38250239+sunwoo76@users.noreply.github.com>
2022-09-19 07:14:39 +0900

    Update solver.py

--- core/solver.py
+++ core/solver.py
@@ -1,13 +1,3 @@
-"""
-StarGAN v2
-Copyright (c) 2020-present NAVER Corp.
-
-This work is licensed under the Creative Commons Attribution-NonCommercial
-4.0 International License. To view a copy of this license, visit
-http://creativecommons.org/licenses/by-nc/4.0/ or send a letter to
-Creative Commons, PO Box 1866, Mountain View, CA 94042, USA.
-"""
-
 import os
 from os.path import join as ospj
 import time
@@ -42,7 +32,7 @@ class Solver(nn.Module):
         self.device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
         self.template, self.prompt, self.prompt_idx, args.num_domains, self.base_template = self._set_attr_prompt(self.args)
-        self.prompt = [self.template.format(x) for x in self.prompt] # template과 결합하여 문장으로 만듬. ex) ["a photo of the fox." , ... , ]
+        self.prompt = [self.template.format(x) for x in self.prompt]
 
         """ network definition """
         self.nets, self.nets_ema = build_model(self.args)
