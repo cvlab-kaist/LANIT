6685ba2f09a9f9ef4d5606f87e317cdc7d840313
SunwooKim <38250239+sunwoo76@users.noreply.github.com>
2022-09-19 06:49:51 +0900

    Update cluster_1.py

--- CUT_CLIP/cluster_1.py
+++ CUT_CLIP/cluster_1.py
@@ -26,34 +26,27 @@ def set_input(input):
     label = input["label"]
     return real_A, image_path, label
 
-""" start, 여기만 바꿔주세요!, promptlearner에서 추출하고 싶으면 class_prompt선언이 끝난 후 코드(약 97번째 이후.)를 봐주세요! """
-
-""" dataset"""
-use_promptLearner = False #(97번째 줄 이후 모두 주석 풀면 자동으로 True로 바뀜.)
-
-# dataset = "celeb" # animal, food, celeb : 데이터셋 이름.
+# dataset = "celeb" # animal, food, celeb : dataset name
 # n = 4 # n= 4, 7, 10 # num_domains
-# dir_A = "/root/project/datasets/food"#"/root/project/datasets/CelebAMask-HQ/celeba" # 데이터셋 경로.
-# save_root = "./clip_celeb_4_PL74000" # 저장할 디렉토리 경로.
-
-# dataset = "animal" # animal, food, celeb : 데이터셋 이름.
-# n = 10 # n= 4, 7, 10 # num_domains
-# dir_A = "/root/project/datasets/af_10" #"/root/project/datasets/CelebAMask-HQ/celeba" # 데이터셋 경로.
-# save_root = "./clip_af_10_PL44000"      # 저장할 디렉토리 경로.
-
-dataset = "celeb" # animal, food, celeb : 데이터셋 이름.
-n = 7 # n= 4, 7, 10 # num_domains
-dir_A = "/root/project/datasets/CelebAMask-HQ/celeba"#"/root/project/datasets/CelebAMask-HQ/celeba" # 데이터셋 경로.
-save_root = "./clip_celeb_7_PL091000" # 저장할 디렉토리 경로.
+# dir_A = "/root/project/datasets/food"#"/root/project/datasets/CelebAMask-HQ/celeba" # path of the dataset
+# save_root = "./clip_celeb_4_PL74000" # path to save the clip similarity vectors
 
 # celeba, ffhq, dir_A = "/root/project/datasets/CelebAMask-HQ/celeba"
 # animalface,  dir_A =  "/root/project/datasets/af_10"
 # food10,      dir_A =  "/root/project/datasets/food"
 
-# celebahq 데이터셋 사용할 경우에만 필요.
-ffhq_anno_path = "/root/project/datasets/CelebAMask-HQ/CelebAMask-HQ-attribute-anno.txt"
-""" end """
+""" dataset """
+use_promptLearner = False
+dataset = "celeb"
+n = 7
+dir_A = "/root/project/datasets/CelebAMask-HQ/celeba"
+save_root = "./clip_celeb_7_PL091000"
 
+
+# it is only needed when the dataset is celebahq celebahq .
+ffhq_anno_path = "/root/project/datasets/CelebAMask-HQ/CelebAMask-HQ-attribute-anno.txt"
+
+""" end """
 
 if "animal" in dataset:
     template = 'a photo of the {}.'
@@ -71,7 +64,6 @@ if "animal" in dataset:
 
     base_prompt = ["a photo of the animal face."]
 
-
 elif "food" in dataset:
     template = 'a photo of the {}.'
     if n == 4:
@@ -88,7 +80,6 @@ elif "food" in dataset:
 
 elif "celeb" or "ffhq" in dataset:
     template = 'a face with {}.'
-
     if n == 4:
         # celeb4
         class_prompt = ['blond hair', 'black hair' , 'smiling', 'eyeglasses',] 
@@ -105,21 +96,18 @@ elif "celeb" or "ffhq" in dataset:
     
     base_prompt = ["a face."]
 
-
 """ model """
-""" 모두 주석 처리하거나, 풀거나. :prompyleaner에서 text를 뽑기위해서, model load하는 경우. """
-use_promptLearner = True
-#checkpoint_path = '/root/data/lunit_weight_step2/food-10_step2/074000_nets.ckpt'
-#checkpoint_path = '/root/data/lunit_weight_step2/af-10_step2/044000_nets.ckpt'
-checkpoint_path = '/root/data/lunit_weight_step2/celeb-7_step2/091000_nets.ckpt'
-checkpoint = torch.load(checkpoint_path, map_location='cuda')
-promptLearner = torch.nn.DataParallel(PromptLearner(device="cuda", K=len(class_prompt), init_prompt=template, classes=class_prompt, clip_model=clip_model, rand_token_len=4))
-promptLearner.module.load_state_dict(checkpoint["promptLearner"])
+""" If you use PromptLearner(in step2), do not annotating line 101 to 106, otherwise annotate all the line from 101 to 106. """
+# use_promptLearner = True
+# checkpoint_path = '/root/data/lunit_weight_step2/celeb-7_step2/091000_nets.ckpt'
+# checkpoint = torch.load(checkpoint_path, map_location='cuda')
+# promptLearner = torch.nn.DataParallel(PromptLearner(device="cuda", K=len(class_prompt), init_prompt=template, classes=class_prompt, clip_model=clip_model, rand_token_len=4))
+# promptLearner.module.load_state_dict(checkpoint["promptLearner"])
 """ end """
 
 prompt = [template.format(x) for x in class_prompt]
 
-""" option 설정 """
+
 opt = TestOptions().parse()
 opt.__setattr__("dataset", dataset)
 opt.__setattr__("dir_A", dir_A)
@@ -129,7 +117,6 @@ dataset = create_dataset(opt)
 print(opt)
 
 with torch.no_grad():
-    """ 모든 topk에 대해서 조사."""
     topk_all_sim_list  = np.array([ 0 for i in range(len(class_prompt)) ])
     topk_all_imgpath_list = np.array([ 0 for i in range(len(class_prompt)) ])
 
@@ -143,12 +130,9 @@ with torch.no_grad():
     topk_perclass_imgpath_list  = [ [ [] for i in range(len(class_prompt))  ] for i in range(len(class_prompt)) ]
 
     """ pre-extract text feature """
-    """ 이미지들만 바뀌기 때문에 매 itreation 마다 text feature를 추출할 필요가 없다. """
     if use_promptLearner:
-        """ prompt learner 이용하는 경우"""
         text_feature = promptLearner(clip_model)
         text_feature = text_feature / text_feature.norm(dim=-1, keepdim=True)
-        """ end """
     else:
         tokens = clip.tokenize(prompt).to(device)
         text_feature = clip_model.encode_text(tokens).detach()
@@ -167,7 +151,7 @@ with torch.no_grad():
         logit_scale = clip_model.logit_scale.exp()
         logits_per_image  = logit_scale * image_features @ text_feature.t() # (1, 7)
 
-        """ base template과 비교 """
+        """ comparing to base template """
         logits_per_image_base  = logit_scale * image_features @ base_text_feature.t() # (1, 7)
 
         probs = logits_per_image.softmax(dim=-1).cpu().numpy()
@@ -183,15 +167,15 @@ with torch.no_grad():
             idx = int(topk_idx[k].item())
             val = topk_val[k].item()
 
-            """ topk번째, similarity value 저장. """
+            """ save topk'th similarity value. """
             topk_perclass_sim_list[idx][k].append(val) # 도메인당, topk구별해서 또 저장.
             topk_perclass_rel_sim_list[idx][k].append(val-logits_per_image_base.item()) # 도메인당, topk구별해서 또 저장.
 
-            """ similarity vector 저장. """
+            """ save similarity vectors. """
             topk_perclass_vector_list[idx][k].append(logits_per_image[0].cpu().numpy())
             topk_perclass_rel_vector_list[idx][k].append( (logits_per_image-logits_per_image_base)[0].cpu().numpy())
   
-            """ img path 저장 """
+            """ save img paths """
             topk_perclass_imgpath_list[idx][k].append(image_path)
 
 os.makedirs(save_root, exist_ok=True)
