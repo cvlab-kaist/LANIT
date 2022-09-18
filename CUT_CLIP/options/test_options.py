dfc5c0d347f4ee9ce8ae0b571fe51251707d4e1e
SunwooKim <38250239+sunwoo76@users.noreply.github.com>
2022-09-19 06:54:24 +0900

    Update test_options.py

--- CUT_CLIP/options/test_options.py
+++ CUT_CLIP/options/test_options.py
@@ -9,6 +9,7 @@ class TestOptions(BaseOptions):
 
     def initialize(self, parser):
         parser = BaseOptions.initialize(self, parser)  # define shared options
+        parser.add_argument('--dataset', type=str, default='animal_faces', help='dataset name to use')
         parser.add_argument('--results_dir', type=str, default='./results/', help='saves results here.')
         parser.add_argument('--phase', type=str, default='test', help='train, val, test, etc')
         # Dropout and Batchnorm has different behavioir during training and test.
